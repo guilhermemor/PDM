037c29e1f6cfd73c88b915e46597ee3b969994db
Guilherme Morais <103127799+guilhermemor@users.noreply.github.com>
2023-08-22 20:32:21 -0300

    revisao

---
+++ revisao.js
@@ -0,0 +1,34 @@
+// variavel 
+const numero = 10
+numero = 20; // erro
+
+const frutas = [];
+frutas.push("banana");
+
+const pessoas = { id: 1, nome: "Jose" };
+pessoa.sexo = 'M';
+pessoa = {id: 2, nome: "Maria"};
+
+const soma = (a, b) => a + b;
+
+const App = () => {
+      return (
+       <View>
+       </View>
+
+      );
+  }
+
+  // desestruturação
+  const {id, nome} = pessoa;
+
+  //modulos
+  export default pessoa;
+  
+  export {frutas, soma};
+
+  import pessoa from 'revisao';
+
+  import {frutas} from 'revisao'
+
+  import {View} from 'react-native'
